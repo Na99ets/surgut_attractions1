8941eec45efc88537d852248777374b1faa9cfc2
Na99ets <kirillna99et@gmail.com>
2024-05-08 18:03:59 +0530

    ахахахаххаха

---
+++ php/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css\stile.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>surgut_attractions</title>
+</head>
+<body>
+    <?php include "header.php"?>
+    <div>
+        <div class="fon"></div>
+        <div class="substrate"></div>
+        <div class="content">
+            <div class="contentbox">
+                <div class="attraction">
+                    <div class="imgshadow"></div>
+                </div>
+                <div class="infobox">
+                    <div class="title">ПАМЯТНИК ОСНОВАТЕЛЯ</div>
+                    <div class="info">
+                        В самом сердце Сургута 12 июня 2002 г. был установлен символичный памятник, объединяющий прошлое, настоящее и будущее. Речь про отлитые в бронзе на одном из предприятий Петербурга люди, чьими руками строился город. 6,5-метровые фигуры князя и воеводы — это Федор Борятинский и Владимир Аничков, прибывшие в Сибирь по царскому распоряжению для постройки города.
+                        
+                        Фигура казака олицетворяет людей, принимавших участие в строительстве (большинство из которых были казаками), местных жителей и добровольных переселенцев. Священник же символизирует крепкую веру и духовность русского народа. Высота всего памятника — 15 м, а на его изготовление ушло 12 месяцев и 40 тонн бронзы. Под фундаментом заложена капсула-письмо для потомков
+                    </div>
+                    <divc class="buttons">
+                        <a type="button" href="#" class="button mapbutton">Показать на карте
+                        </a>
+                    </div>
+                    <script src="script.js"></script>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "footer.php"?>
+</body>
+</html>
