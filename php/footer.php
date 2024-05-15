ef2edb84f5000707b5f8f46a8425c52b1b9318ec
Na99ets <kirillna99et@gmail.com>
2024-05-16 04:53:53 +0530

    кидаю

--- php/footer.php
+++ php/footer.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="footer">
         <p>
             Главная
             О нас
--- php/header.php
+++ php/header.php
@@ -1,18 +1,18 @@
-<header>
-        <div class="fon1">
-            <div class="krug">
-                <img class="logo" src="img/logo.png" alt="">
-            </div>
-            <h1 class="RuEn">RU/EN</h1>
-            <img class="strelka" src="img/catalog1.png" alt="">
-            <div class="searchline"> 
-                <input type="text" class="search-input" placeholder="Введите запрос...">
-                <!-- <div class="buttons"> -->
-                    <a type="button" href="#" class="button search-button">
-                    </a>
-                <!-- </div> -->
-                
-            </div>
-            <img class="media" src="img/socialmedia.png" alt="">
+<header class="header">
+    <div class="fon1">
+        <div class="krug">
+            <img class="logo" src="img/logo.png" alt="">
         </div>
-    </header>
+        <h1 class="RuEn">RU/EN</h1>
+        <img class="strelka" src="img/catalog1.png" alt="">
+        <div class="searchline"> 
+            <input type="text" class="search-input" placeholder="Введите запрос...">
+            <!-- <div class="buttons"> -->
+                <a type="button" href="#" class="button search-button">
+                </a>
+            <!-- </div> -->
+            
+        </div>
+        <img class="media" src="img/socialmedia.png" alt="">
+    </div>
+</header>
